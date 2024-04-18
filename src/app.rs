c8e5a207ff94651dc683c74029a6596c5227a908
Alex Lazar <alexandazar@gmail.com>
2024-04-18 09:11:48 -0700

    Add some basics

--- src/app.rs
+++ src/app.rs
@@ -2,6 +2,7 @@ use crate::error_template::{AppError, ErrorTemplate};
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
+use serde::{Deserialize, Serialize};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -9,16 +10,8 @@ pub fn App() -> impl IntoView {
     provide_meta_context();
 
     view! {
-
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/svixbar.css"/>
-
-        // sets the document title
-        <Title text="Welcome to Leptos"/>
-
-        // content for this welcome page
+        <Title text="FooBar"/>
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
@@ -36,15 +29,25 @@ pub fn App() -> impl IntoView {
     }
 }
 
-/// Renders the home page of your application.
+#[derive(Debug, Clone, Deserialize, Serialize)]
+pub enum CreateTask {
+    Foo(String),
+    Bar,
+    Baz,
+}
+
+#[server(endpoint = "add_task")]
+pub async fn add_task(task: CreateTask) -> Result<(), ServerFnError> {
+    Ok(())
+}
+
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1>"Task Manager"</h1>
+        <button on:click=on_click>Add Foo Task</button>
     }
 }
